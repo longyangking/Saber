2ee49294ecba1eb815302d8e22e87743e9d039bd
Long Yang <longyang_123@yeah.net>
2016-10-30 11:35:16 +0800

    Init Structure

--- server/server.js
+++ server/server.js
@@ -1,1 +1,11 @@
 var http = require("http");
+var fs = require("fs");
+var path = require("path");
+var process = require("process");
+var querystring = require("querystring");
+var zlib = require("zlib");
+
+var write404 = function(response,callback){
+	var file = "./error/404.html";
+	
+}
